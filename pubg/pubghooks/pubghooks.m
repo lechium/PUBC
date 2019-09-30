1065b983d7bb1c4ad2104c2c4943487c7f17fa44
Kevin Bradley <kevin.w.bradley@me.com>
2019-09-30 15:26:00 -0700

    0.14.0 support

--- pubg/pubghooks/pubghooks.m
+++ pubg/pubghooks/pubghooks.m
@@ -27,55 +27,57 @@ static inline int is_local_player(void *obj){
     return (*(char *)((uint64_t)obj + 0x128) == 2);
 }
 
-static void *(*sub_101c306c0)(void *, float);
+static void *(*sub_101c6fb20)(void *, float);
 
-static void *_sub_101c306c0(void *_this, float arg1){
+static void *_sub_101c6fb20(void *_this, float arg1){
     if(is_local_player(_this)){
-        void *x8 = *(void **)((uint64_t)_this + 0x1190);
-
+        void *x8 = *(void **)((uint64_t)_this + 0x1378);
+        
         if(x8)
             MOVE_TYPE_INTERNAL = *(char *)((uint64_t)x8 + 0x1c4);
     }
-
-    return sub_101c306c0(_this, arg1);
+    
+    return sub_101c6fb20(_this, arg1);
 }
 
-static void (*sub_101be39f0)(void *, char);
+static void (*sub_101c1da54)(void *, char);
 
-static void _sub_101be39f0(void *_this, char arg1){
+static void _sub_101c1da54(void *_this, char arg1){
     IS_AIMED_DOWN_SIGHTS_INTERNAL = *(char *)((uint64_t)_this + 0x114);
 
-    sub_101be39f0(_this, arg1);
+    sub_101c1da54(_this, arg1);
 }
 
-static void (*sub_101c33950)(void *);
+static void (*sub_101c72490)(void *);
 
-static void _sub_101c33950(void *_this){
-    sub_101c33950(_this);
-
+static void _sub_101c72490(void *_this){
+    sub_101c72490(_this);
+    
     if(!is_local_player(_this))
         return;
-
-    void *x8 = *(void **)((uint64_t)_this);
-
+    
+    void *x8 = *(void **)_this;
+    
     if(!x8)
         return;
-
-    x8 = *(void **)((uint64_t)x8 + 0x9a0);
-
+    
+    x8 = *(void **)((uint64_t)x8 + 0x9a8);
+    
     if(!x8)
         return;
-
+    
     void *(*x8_fxnptr)(void *) = (void *(*)(void *))x8;
     void *x0 = x8_fxnptr(_this);
-
+    
     if(!x0)
         return;
-
-    int w8 = 0x2808;
-
+    
+    int w8 = 0x2948;
+    
     IS_PRESSING_FIRE_BUTTON_INTERNAL = *(char *)((uint64_t)x0 + w8);
 }
+
+static void *(*GetEntity)(void *_this) = NULL;
 
 #define HOOK(offset, ptr, orig) \
 MSHookFunction((void *)(offset + slide), (void *)ptr, (void **)&orig)
@@ -84,16 +86,21 @@ void ph_initialize(void){
 
     NSString *version = [[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"];
     NSComparisonResult result
-    = [@"0.13.0" compare:version
+    = [@"0.14.0" compare:version
                     options:NSNumericSearch | NSCaseInsensitiveSearch];
     if (result == NSOrderedSame){
+        
+        
         
         HOOKER_INTERNAL = true;
         NSLog(@"PUBC: WHAT ARE THOSE, HOOKS? GET OUT OF HERE YA BUNCH OF HOOKERS!");
         uint64_t slide = _dyld_get_image_vmaddr_slide(0);
-        HOOK(0x101c306c0, _sub_101c306c0, sub_101c306c0);
-        HOOK(0x101be39f0, _sub_101be39f0, sub_101be39f0);
-        HOOK(0x101c33950, _sub_101c33950, sub_101c33950);
+        
+        GetEntity = (void *(*)(void *))(slide + 0x101c62210);
+        
+        HOOK(0x101c6fb20, _sub_101c6fb20, sub_101c6fb20);
+        HOOK(0x101c1da54, _sub_101c1da54, sub_101c1da54);
+        HOOK(0x101c72490, _sub_101c72490, sub_101c72490);
     }
     
 }
